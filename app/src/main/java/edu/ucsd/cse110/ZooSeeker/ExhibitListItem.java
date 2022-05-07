578c5baf397a00015542a2ea5a07fc51928d4700
Yuxuan-Wu <charleslele2002@gmail.com>
2022-05-07 11:04:02 -0700

    Added new class to contain list items updated

--- app/src/main/java/edu/ucsd/cse110/ZooSeeker/ExhibitListItem.java
+++ app/src/main/java/edu/ucsd/cse110/ZooSeeker/ExhibitListItem.java
@@ -1,8 +1,25 @@
 package edu.ucsd.cse110.ZooSeeker;
 
+import androidx.annotation.NonNull;
 import androidx.room.*;
 
 @Entity(tableName = "exhibit_list_items")
 public class ExhibitListItem {
+    //public fields
 
+    @PrimaryKey(autoGenerate = true)
+    public long id;
+
+    @NonNull
+    public String exhibitName;
+    public boolean selected;
+    public int order;
+
+    //constructor matching fields above
+
+    public ExhibitListItem(@NonNull String exhibitName, boolean selected, int order) {
+        this.exhibitName = exhibitName;
+        this.selected = selected;
+        this.order = order;
+    }
 }
