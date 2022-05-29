f707577547f92e8466df4b884b0ed0515458ccbf
gd23452008@gmail.com <gd23452008@gmail.com>
2022-05-28 23:03:11 -0700

    refactor find direction file

--- app/src/main/java/edu/ucsd/cse110/ZooSeeker/FindDirection.java
+++ app/src/main/java/edu/ucsd/cse110/ZooSeeker/FindDirection.java
@@ -16,6 +16,8 @@ import static edu.ucsd.cse110.ZooSeeker.SearchListActivity.sortedID;
 
 import android.location.Location;
 
+import androidx.annotation.NonNull;
+
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
@@ -28,11 +30,9 @@ public class FindDirection {
     public static String printPath(String start, String goal){
         GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(graphInfoMap, start, goal);
 
-        //future feature for real location
-        String rlt = "You are close to " + findNearestExhibitID(currLocation) + ". ";
-        //String rlt = "" + testLong + " " + testLati + " ";
-        //rlt += "You are close to " + findNearestExhibitID() + " ";
-
+        //Construct Path information
+        String rlt = "You are close to " +
+                findNearestExhibitID(currLocation) + ". ";
         for (IdentifiedWeightedEdge e : path.getEdgeList()) {
             String s = "Walk " + (int)graphInfoMap.getEdgeWeight(e)
                     + " feet along " + edgeInfoMap.get(e.getId()).street + ". ";
@@ -42,101 +42,63 @@ public class FindDirection {
     }
 
     public static String printDistance(String start, String goal){
-        GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(graphInfoMap, start, goal);
+        GraphPath<String, IdentifiedWeightedEdge> path =
+                DijkstraShortestPath.findPathBetween(graphInfoMap, start, goal);
 
+        //get the total weight and construct and return it
+        double weight = getTotalWeight(path);
+        String rlt = IDToNameMap.get(goal) + ", " + (int)weight + " feet away.";
+        return rlt;
+    }
+
+    private static double getTotalWeight(GraphPath<String, IdentifiedWeightedEdge> path) {
         double weight = 0;
-
         for (IdentifiedWeightedEdge e : path.getEdgeList()) {
             weight += graphInfoMap.getEdgeWeight(e);
         }
-
-        String rlt = IDToNameMap.get(goal) + ", " + (int)weight + " feet away.";
-
-        return rlt;
-    }
-    
-    public static String findNearestExhibitID() {
-        String result = "";
-        double min = Integer.MAX_VALUE;
-
-        // Todo: 监视（call这个function，如果result！= cur 弹提示框问点不点，
-        // TOdo: 按了replan就改变cur to result） + 提示框 + replan button
-
-        //loop to get the shortest one from all the node
-        for( String key : vertexInfoMap.keySet() ){
-            ZooData.VertexInfo exhibitInfo = vertexInfoMap.get(key);
-            Location endPoint=new Location(exhibitInfo.id);
-            endPoint.setLatitude(exhibitInfo.lat);
-            endPoint.setLongitude(exhibitInfo.lng);
-
-            //test
-            {
-                //    "lat": 32.74505139995802,
-                //    "lng": -117.15794384136309
-                //TEST flamingo
-
-                Location testPoint = new Location("TEST");
-                testPoint.setLatitude(32.746302644092815);
-                testPoint.setLongitude(-117.15794384136309);
-
-                /* "lat": 32.746302644092815,
-                "lng": -117.16659525430192*/
-
-                //double distance=testPoint.distanceTo(endPoint);
-            }
-
-            //calculate shortest one and record, return it
-            double distance=curLocation.distanceTo(endPoint);
-            if(distance < min){
-                min = distance;
-                System.out.println(result);
-                result = exhibitInfo.name;
-            }
-        }
-        return result;
+        return weight;
     }
 
     public static String findNearestExhibitID( Location location) {
         String result = "";
-        double min = Integer.MAX_VALUE;
-
-        // Todo: 监视（call这个function，如果result！= cur 弹提示框问点不点，
-        // TOdo: 按了replan就改变cur to result） + 提示框 + replan button
 
         //loop to get the shortest one from all the node
         for( String key : vertexInfoMap.keySet() ){
             ZooData.VertexInfo exhibitInfo = vertexInfoMap.get(key);
-            Location endPoint=new Location(exhibitInfo.id);
-            endPoint.setLatitude(exhibitInfo.lat);
-            endPoint.setLongitude(exhibitInfo.lng);
-
+            Location endPoint = setLocation(exhibitInfo.id, exhibitInfo.lat, exhibitInfo.lng);
             //test
             {
                 //    "lat": 32.74505139995802,
                 //    "lng": -117.15794384136309
                 //TEST flamingo
-
-                Location testPoint = new Location("TEST");
-                testPoint.setLatitude(32.746302644092815);
-                testPoint.setLongitude(-117.15794384136309);
-
+                Location testPoint = setLocation("TEST", 32.746302644092815, -117.15794384136309);
                 /* "lat": 32.746302644092815,
                 "lng": -117.16659525430192*/
-
                 //double distance=testPoint.distanceTo(endPoint);
             }
-
             //calculate shortest one and record, return it
-            double distance=location.distanceTo(endPoint);
-            if(distance < min){
-                min = distance;
-                System.out.println(result);
-                result = exhibitInfo.name;
-            }
+            result = getShortestExhibitId(location, result, exhibitInfo, endPoint);
         }
 
-        result = nameToParentIDMap.get(result);
+        return result;
+    }
 
+    @NonNull
+    private static Location setLocation(String id, double lat, double lng) {
+        Location endPoint = new Location(id);
+        endPoint.setLatitude(lat);
+        endPoint.setLongitude(lng);
+        return endPoint;
+    }
+
+    private static String getShortestExhibitId(Location location, String result, ZooData.VertexInfo exhibitInfo, Location endPoint) {
+        double min = Integer.MAX_VALUE;
+        if(location.distanceTo(endPoint) < min){
+            min = location.distanceTo(endPoint);
+            System.out.println(result);
+            result = exhibitInfo.name;
+        }
+        result = nameToParentIDMap.get(result);
         return result;
     }
 }
